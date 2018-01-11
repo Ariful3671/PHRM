dee45c4d69580d1070221cea995ba41c518bbc88
Arif <ariful3671@gmail.com>
2018-01-11 22:26:09 +0600

    New

--- app/src/main/java/com/troubleshooters/diu/phrm/NutritionActivity.java
+++ app/src/main/java/com/troubleshooters/diu/phrm/NutritionActivity.java
@@ -111,8 +111,8 @@ public class NutritionActivity extends AppCompatActivity implements TimePickerDi
 
 
         Calendar calendar=Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY ,22);
-        calendar.set(Calendar.MINUTE,14);
+        calendar.set(Calendar.HOUR_OF_DAY ,24);
+        calendar.set(Calendar.MINUTE,0);
         Intent intent=new Intent(getApplicationContext(),ResetApp.class);
         PendingIntent pendingIntent=PendingIntent.getBroadcast(getApplicationContext(),1000,intent,PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarmManager=(AlarmManager)getSystemService(ALARM_SERVICE);
