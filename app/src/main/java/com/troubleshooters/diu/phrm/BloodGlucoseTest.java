fc7bc9304cd9922d48e5fe6bb84bf46aa8cd9335
Oahid <taninwahid104@gmail.com>
2018-02-07 08:28:35 +0600

    Exercise Activity

---
+++ app/src/main/java/com/troubleshooters/diu/phrm/BloodGlucoseTest.java
@@ -0,0 +1,13 @@
+package com.troubleshooters.diu.phrm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BloodGlucoseTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blood_glucose_test);
+    }
+}
